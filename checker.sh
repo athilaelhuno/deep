397152c723fe02d61c13c7304ae1179590a7b662
Enrique Fermin <efermin@aaxiscommerce.com>
2018-08-02 17:02:29 -0400

    Added files

--- checker.sh
+++ checker.sh
@@ -6,7 +6,7 @@ while :
 do
 	ps_out=`ps -ef | grep $1 | grep -v 'grep' | grep -v $0`
 	result=$(echo $ps_out | grep "$1")
-	if [[ "$result" != "" ]];then
+	if [ "$result" != "" ];then
 	    echo "Running"
 	else
 	    echo "Not Running"
