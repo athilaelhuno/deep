9cbf1a785729de980bef3bd4aa2663b0b9bfea5c
Enrique Fermin <efermin@aaxiscommerce.com>
2018-08-02 16:30:00 -0400

    Added files

--- checker.sh
+++ checker.sh
@@ -5,13 +5,12 @@ PROCANDARGS=$*
 
 while :
 do
-	ps_out=`ps -ef | grep $1 | grep -v 'grep' | grep -v $0`
-	result=$(echo $ps_out | grep "$1")
-	if [[ "$result" != "" ]];then
-	    echo "Running"
-	else
-	    echo "Not Running"
-	    sudo init 0
-	fi
+        ps_out=`ps -ef | grep $1 | grep -v 'grep' | grep -v $0`
+        result=$(echo $ps_out | grep "$1")
+        if [ "$result" != "" ];then
+            echo "Running"
+        else
+            echo "Not Running"
+        fi
   sleep 5
 done
--- process.py
+++ process.py
@@ -2,6 +2,6 @@
 from time import sleep
 
 
-for x in xrange(1,70):
-	sleep(1)
-	print x
+for x in range(1,70):
+        sleep(1)
+        print (x)
