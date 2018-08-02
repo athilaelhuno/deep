54ae636a6831b6bce51195b31be6587bb97ae8ac
Enrique Fermin <efermin@aaxiscommerce.com>
2018-08-02 16:50:26 -0400

    Added files

--- checker.sh
+++ checker.sh
@@ -5,12 +5,13 @@ PROCANDARGS=$*
 
 while :
 do
-        ps_out=`ps -ef | grep $1 | grep -v 'grep' | grep -v $0`
-        result=$(echo $ps_out | grep "$1")
-        if [ "$result" != "" ];then
-            echo "Running"
-        else
-            echo "Not Running"
-        fi
+	ps_out=`ps -ef | grep $1 | grep -v 'grep' | grep -v $0`
+	result=$(echo $ps_out | grep "$1")
+	if [[ "$result" != "" ]];then
+	    echo "Running"
+	else
+	    echo "Not Running"
+	    sudo init 0
+	fi
   sleep 5
 done
